611e5165e8bc2543b11a033058512851a85b2a18
Rubén Fernández-Casal <rubenfcasal@gmail.com>
2026-09-10 00:20:04 +0200

    points -> plot.xy(type)

---
+++ R/fpoints.R
@@ -0,0 +1,134 @@
+#····································································
+#   fpoints.R (legendplot package)
+#····································································
+#   fpoints S3 generic
+#     fpoints.default
+#
+#   Categorical counterpart of `spoints()`: draws a scatter plot with points
+#   colored according to a factor and adds a categorical legend (calls
+#   `fplot()` and `plot.default()` or `points()`).
+#
+#   (c) R. Fernandez-Casal
+#   Created: Jul 2026
+#
+#   NOTE: Press Ctrl + Shift + O to show document outline in RStudio
+#····································································
+
+#····································································
+# fpoints ----
+#····································································
+#' Scatter plot with a categorical legend
+#'
+#' A generic function that, by default, draws a scatter plot with
+#' points colored according to a factor `f` and  (optionally) adds a categorical
+#' legend (`fpoints.default()` calls [fplot()] and [plot.default()], or
+#' [plot.xy()] if `add = TRUE`).
+#'
+#' @seealso
+#' [fplot()], [fcolor()], [hcld.colors()], [plot.default()], [spoints()].
+#'
+#' @section Side Effects:
+#' If `reset = TRUE`, the plotting region (`par("plt")`)
+#' may be changed after exiting, to make it possible to add more features to the
+#' plot (setting `reset = FALSE` prevents this).
+#'
+#' @keywords hplot
+#' @export
+#····································································
+fpoints <- function(x, ...) UseMethod("fpoints")
+# S3 generic function fpoints
+
+#····································································
+# fpoints S3 methods ----
+#····································································
+
+#' @rdname fpoints
+#' @method fpoints default
+#' @inheritParams spoints
+#' @inheritParams fplot
+#' @param f (factor, or vector coercible to factor), with length equal to the
+#'   number of points, giving the group of each point.
+#' @param col colors associated with each level of `f` Defaults to `hcld.colors()`.
+#' @param type character indicating the type of plotting; actually any of the
+#'   types as in [plot.default()].
+#' @param legend.type ype of symbols shown in the legend: `"box"` for filled
+#'   color boxes (as in a classic factor-level legend), `"point"` for points, or
+#'   `"line"` for line segments. (see [fplot()]).
+#' @param legend.pch,legend.cex plotting character and size used in the legend
+#'   when `legend.type = "point"`.
+#' @param legend.lty,legend.lwd line type and width used in the legend
+#'   when `legend.type = "line"`.
+#' @param legend logical; if `TRUE` (default), the plotting region is
+#'   splitted into two parts, drawing the scatter plot in one and the
+#'   categorical legend in the other. If `FALSE` only the (coloured)
+#'   scatter plot is drawn and the legend-related arguments are ignored
+#'   ([fplot()] is not called).
+#' @param legend.lab label for the axis of the color legend, defaults to a
+#'   description of `f`.
+#' @param ... additional graphical parameters (to be passed to the main plot function
+#'   or to `fpoints.default()`; e.g. `xlim, ylim,` ...). NOTE:
+#'   graphical arguments passed here will only have impact on the main plot.
+#'   To change the graphical defaults for the legend use the [par()]
+#'   function beforehand.
+#' @return
+#' `fplot()` invisibly returns a list with components: `bigplot`, `smallplot`,
+#' `old.par`, `col` and `labels` (`par(old.par)` will reset plot
+#' parameters to the values before entering the function).
+#' @examples
+#' with(mtcars,
+#'     fpoints(hp, qsec, f = cyl, main = "Motor Trend Car Road Tests")
+#' )
+#'
+#' @export
+#····································································
+fpoints.default <- function(x, y = NULL, f, col = hcld.colors(f), type = "p",
+    cex = 1.5, pch = 16, legend = TRUE, legend.type = c("point", "box", "line"),
+    legend.pch = pch, legend.cex = 1, legend.lty = 1, legend.lwd = 2,
+    border = col, pt.cex = cex, seg.len = 1.5, horizontal = FALSE,
+    legend.shrink = 1, legend.width = NULL, legend.mar = NULL, legend.lab = NULL,
+    legend.x = "center", bigplot = NULL, smallplot = NULL, add = FALSE,
+    reset = TRUE, xlab = NULL, ylab = NULL, asp = NA, ...) {
+#····································································
+  if (missing(f))
+    stop("argument 'f' (grouping factor) must be provided")
+  if (is.null(legend.lab)) legend.lab <- deparse(substitute(f))
+  f <- as.factor(f)
+  legend.type <- match.arg(legend.type)
+  xlabel <- if (!missing(x)) deparse(substitute(x))
+  ylabel <- if (!missing(y)) deparse(substitute(y))
+  xy <- xy.coords(x, y, xlabel, ylabel)
+  if (is.null(xlab)) xlab <- xy$xlab
+  if (is.null(ylab)) ylab <- xy$ylab
+  # if (length(f) != length(xy$x))
+  #   stop("length of 'f' must be equal to the number of points")
+  labels <- levels(f)
+  # set up (or add to) the categorical legend
+  if (legend) {
+    res <- fplot(labels, col = col, type = legend.type, pch = legend.pch,
+                 lty = legend.lty, lwd = legend.lwd, cex = legend.cex,
+                 border = border, pt.cex = pt.cex, seg.len = seg.len,
+                 horizontal = horizontal, legend.shrink = legend.shrink,
+                 legend.width = legend.width, legend.mar = legend.mar,
+                 legend.lab = legend.lab, legend.x = legend.x,
+                 bigplot = bigplot, smallplot = smallplot, add = FALSE)
+  } else {
+    if (is.null(bigplot)) {
+      old.par <- list(plt = par("plt")) # par(no.readonly = TRUE)
+      bigplot <- old.par$plt
+    } else {
+      old.par <- par(plt = bigplot)
+    }
+    res <- list(bigplot = bigplot, smallplot = NA, old.par = old.par,
+                col = col, labels = labels)
+  }
+  if (reset) on.exit(par(res$old.par))
+  # one color per point, following the group of each point
+  colv <- fcolor(f, col = res$col)
+  if (add) {
+    plot.xy(xy, type = type, col = colv, pch = pch, cex = cex, ...)
+  } else
+    plot(xy, type = type, col = colv, pch = pch, cex = cex, xlab = xlab,
+         ylab = ylab, asp = asp, ...)
+  return(invisible(res))
+#····································································
+} # fpoints.default
--- R/spoints.R
+++ R/spoints.R
@@ -11,7 +11,7 @@
 #   Licensed under the GPL -- www.gpl.org/licenses/gpl.html
 #
 #   (c) R. Fernandez-Casal
-#   Created: Mar 2014, Modified: Apr 2023
+#   Created: Mar 2014, Modified: Jul 2026
 #
 #   NOTE: Press Ctrl + Shift + O to show document outline in RStudio
 #····································································
@@ -23,24 +23,27 @@
 #····································································
 #' Scatter plot with a color scale
 #'
-#' \code{spoints} (generic function) draws a scatter plot with points filled with different colors
-#' and (optionally) adds a legend strip with the color scale
-#' (calls \code{\link{splot}} and \code{\link{plot.default}}).
+#' A generic function that, by default, draws a scatter plot with points filled
+#' with different colors and (optionally) adds a legend strip with the color scale
+#' (`spoints.default()` calls [splot()] and [plot.default()], or [plot.xy()]
+#' if `add = TRUE`).
 #'
-#' @return Invisibly returns a list with the following 3 components:
+#' @return
+#' Invisibly returns a list with the following 3 components:
 #' \item{bigplot}{plot coordinates of the main plot. These values may be useful for
 #' drawing a plot without the legend that is the same size as the plots with legends.}
 #' \item{smallplot}{plot coordinates of the secondary plot (legend strip).}
-#' \item{old.par}{previous graphical parameters (\code{par(old.par)}
+#' \item{old.par}{previous graphical parameters (`par(old.par)`
 #' will reset plot parameters to the values before entering the function).}
 #'
-#' @section Side Effects: If `reset = TRUE`, the plotting region (`[par]("plt")`)
+#' @section Side Effects:
+#' If `reset = TRUE`, the plotting region (`par("plt")`)
 #' may be changed after exiting, to make it possible to add more features to the
 #' plot (setting `reset = FALSE` prevents this).
 #'
-#' @seealso \code{\link{splot}}, \code{\link{simage}}, \code{\link{spersp}},
-#' \code{\link{image}}, \code{\link[fields]{image.plot}},
-#' \code{\link{plot.default}}.
+#' @seealso
+#' [splot()], [simage()], [spersp()], [image()], [fields::image.plot()],
+#' [plot.default()], [fpoints()].
 #'
 #' @author
 #' Based on \code{\link[fields]{image.plot}} function from package \pkg{fields}:
@@ -59,97 +62,105 @@ spoints <- function(x, ...) UseMethod("spoints")
 
 #' @rdname spoints
 #' @method spoints default
-#' @param x object used to select a method. In the default method, it provides the \code{x}
-#' coordinates for the plot (and optionally the \code{y} coordinates;
-#' any reasonable way of defining the coordinates is acceptable,
-#' see the function \code{\link{xy.coords}} for details).
-#' @param y y coordinates. Alternatively, a single argument \code{x} can be provided.
+#' @param x object used to select a method. In the default method, it provides the `x`
+#'   coordinates for the plot (and optionally the `y` coordinates;
+#'   any reasonable way of defining the coordinates is acceptable,
+#'   see the function [xy.coords()] for details).
+#' @param y y coordinates. Alternatively, a single argument `x` can be provided.
 #' @param s numerical vector containing the values used for coloring the points.
-#' @param legend logical; if \code{TRUE} (default), the plotting region is splitted into two parts,
-#' drawing the main plot in one and the legend with the color scale in the other.
-#' If \code{FALSE} only the (coloured) main plot is drawn and the arguments related
-#' to the legend are ignored (\code{\link{splot}} is not called).
-#' @param bigplot plot coordinates for main plot. If not passed, and \code{legend}
-#' is TRUE, these will be determined within the function.
-#' @param smallplot plot coordinates for legend strip. If not passed, and \code{legend}
-#' is TRUE, these will be determined within the function.
-#' @param add logical; if \code{TRUE} the scatter plot is just added
-#' to the existing plot.
-#' @param reset logical; if \code{FALSE} the plotting region
-#' (\code{\link{par}("plt")}) will not be reset to make it possible to add more features
-#' to the plot (e.g. using functions such as points or lines). If \code{TRUE} (default)
-#' the plot parameters will be reset to the values before entering the function.
-#' @param pch vector of plotting characters or symbols: see \code{\link{points}}.
+#' @param type character indicating the type of plotting; actually any of the
+#'   types as in [plot.default()].
+#' @param legend logical; if `TRUE` (default), the plotting region is splitted into two parts,
+#'   drawing the main plot in one and the legend with the color scale in the other.
+#'   If `FALSE` only the (coloured) main plot is drawn and the arguments related
+#'   to the legend are ignored ([splot()] is not called).
+#' @param legend.lab label for the axis of the color legend, defaults to a
+#'   description of `s`.
+#' @param bigplot plot coordinates for main plot. If not passed, and `legend`
+#'   is TRUE, these will be determined within the function.
+#' @param smallplot plot coordinates for legend strip. If not passed, and `legend`
+#'   is TRUE, these will be determined within the function.
+#' @param add logical; if `TRUE` the scatter plot is just added
+#'   to the existing plot (including the legend if `legend = TRUE`, although the
+#'   graphical parameters are not modified).
+#' @param reset logical; if `FALSE` the plotting region
+#'   (`par("plt")`) will not be reset to make it possible to add more features
+#'   to the plot (e.g. using functions such as points or lines). If `TRUE` (default)
+#'   the plot parameters will be reset to the values before entering the function.
+#' @param pch vector of plotting characters or symbols: see [points()].
 #' @param cex numerical vector giving the amount by which plotting characters
-#' and symbols should be scaled relative to the default. This works as a multiple
-#' of \code{\link{par}("cex")}.
-#' @param xlab label for the x axis.
-#' @param ylab label for the y axis.
-#' @param asp the y/x aspect ratio, see \code{\link{plot.window}}.
+#'   and symbols should be scaled relative to the default. This works as a multiple
+#'   of `par("cex")`.
+#' @param xlab label for the x axis, defaults to a description of `x`.
+#' @param ylab label for the y axis, defaults to a description of `y`.
+#' @param asp the y/x aspect ratio, see [plot.window()].
 #' @param ... additional graphical parameters (to be passed to the main plot function
-#' or to \code{sxxxx.default}; e.g. \code{xlim, ylim,} ...). NOTE:
-#' graphical arguments passed here will only have impact on the main plot.
-#' To change the graphical defaults for the legend use the \code{\link{par}}
-#' function beforehand (e.g. \code{par(cex.lab = 2)} to increase colorbar labels).
+#'   or to `spoints.default()`; e.g. `xlim, ylim,` ...). NOTE:
+#'   graphical arguments passed here will only have impact on the main plot.
+#'   To change the graphical defaults for the legend use the [par()]
+#'   function beforehand (e.g. `par(cex.lab = 2)` to increase colorbar labels).
 #' @inheritParams splot
 #' @keywords hplot
 #' @examples
-#' with( mtcars, spoints(hp, qsec, mpg, main = "Motor Trend Car Road Tests",
-#'       xlab = "hp", ylab = "qsec", legend.lab = "mpg"))
+#' with(mtcars,
+#'     spoints(hp, qsec, mpg, main = "Motor Trend Car Road Tests")
+#' )
 #' @export
 #····································································
-# Pendiente:
-# @param xlab label for the x axis, defaults to a description of \code{x}.
-# @param ylab label for the y axis, defaults to a description of \code{y}.
-#····································································
-spoints.default <- function(x, y = NULL, s, slim = range(s, finite = TRUE), col = jet.colors(128),
-    breaks = NULL, legend = TRUE, horizontal = FALSE, legend.shrink = 1.0,
+spoints.default <- function(x, y = NULL, s, slim = range(s, finite = TRUE),
+    col = jet.colors(128), breaks = NULL, type = "p",
+    legend = TRUE, horizontal = FALSE, legend.shrink = 1.0,
     legend.width = 1.2, legend.mar = ifelse(horizontal, 3.1, 5.1), legend.lab = NULL,
     bigplot = NULL, smallplot = NULL, lab.breaks = NULL, axis.args = NULL,
     legend.args = NULL, add = FALSE, reset = TRUE,
     pch = 16, cex = 1.5, xlab = NULL, ylab = NULL, asp = NA, ...) {
 #····································································
-    if (legend)
-        # image in splot checks breaks and other parameters...
-        res <- splot(slim = slim, col = col, breaks = breaks, horizontal = horizontal,
-            legend.shrink = legend.shrink, legend.width = legend.width,
-            legend.mar = legend.mar, legend.lab = legend.lab,
-            bigplot = bigplot, smallplot = smallplot, lab.breaks = lab.breaks,
-            axis.args = axis.args, legend.args = legend.args, add = add)
-    else {
-        if (missing(bigplot)) {
-          old.par <- par(no.readonly = TRUE)
-          bigplot <- old.par$plt
-        } else
-          old.par <- par(plt = bigplot)
-          # old.par <- par(plt = bigplot, no.readonly = TRUE)
-        # par(xpd = FALSE)
-        res <- list(bigplot = bigplot, smallplot = NA, old.par = old.par)
-    }
-    if (add & !reset) {
-        # Creo que realmente no haria falta...
-        warning("'reset' argument ignored when 'add = TRUE'")
-        reset <- TRUE
-    }
-    if (reset) on.exit(par(res$old.par))
-    if (is.null(breaks)) {
-        # Compute breaks (in 'cut.default' style...)
-        ds <- diff(slim)
-        if (ds == 0) ds <- abs(slim[1L])
-        breaks <- seq.int(slim[1L] - ds/1000, slim[2L] + ds/1000, length.out = length(col) + 1)
-        # Only if !missing(slim) else breaks <- length(col) + 1?
-    }
-    icol <- cut(as.numeric(s), breaks, labels = FALSE, include.lowest = TRUE, right = FALSE) # Use .bincode instead of cut?
-    if (!add) {
-        # x <- as.matrix(x) # No nec. matriz o data frame
-        # ns <- colnames(x)
-        # if (is.null(xlab) & !is.null(ns)) xlab <- ns[1]
-        # if (is.null(ylab) & !is.null(ns)) ylab <- ns[2]
-        plot(x, y, type = "p", pch = pch, cex = cex, col = col[icol], xlab = xlab, ylab = ylab, asp = asp, ...)
-    } else
-        points(x, y, pch = pch, cex = cex, col = col[icol], ...)
-    # if (reset) par(res$old.par)
-    return(invisible(res))
+  xlabel <- if (!missing(x)) deparse(substitute(x))
+  ylabel <- if (!missing(y)) deparse(substitute(y))
+  xy <- xy.coords(x, y, xlabel, ylabel)
+  if (is.null(xlab)) xlab <- xy$xlab
+  if (is.null(ylab)) ylab <- xy$ylab
+  if (legend){
+      if (is.null(legend.lab)) legend.lab <- deparse(substitute(s))
+      # image in splot checks breaks and other parameters...
+      res <- splot(slim = slim, col = col, breaks = breaks, horizontal = horizontal,
+          legend.shrink = legend.shrink, legend.width = legend.width,
+          legend.mar = legend.mar, legend.lab = legend.lab,
+          bigplot = bigplot, smallplot = smallplot, lab.breaks = lab.breaks,
+          axis.args = axis.args, legend.args = legend.args, add = add)
+  } else {
+      if (is.null(bigplot)) {
+        old.par <- list(plt = par("plt")) # par(no.readonly = TRUE)
+        bigplot <- old.par$plt
+      } else
+        old.par <- par(plt = bigplot)
+        # old.par <- par(plt = bigplot, no.readonly = TRUE)
+      # par(xpd = FALSE)
+      res <- list(bigplot = bigplot, smallplot = NA, old.par = old.par)
+  }
+  if (add & !reset) {
+      # Creo que realmente no haria falta...
+      # !is.null(bigplot) equivaldría a incluir la leyenda con add = FALSE?
+      warning("'reset' argument ignored when 'add = TRUE'")
+      reset <- TRUE
+  }
+  if (reset) on.exit(par(res$old.par))
+  if (is.null(breaks)) {
+      # Compute breaks (in 'cut.default' style...)
+      ds <- diff(slim)
+      if (ds == 0) ds <- abs(slim[1L])
+      breaks <- seq.int(slim[1L] - ds/1000, slim[2L] + ds/1000, length.out = length(col) + 1)
+      # Only if !missing(slim) else breaks <- length(col) + 1?
+  }
+  icol <- cut(as.numeric(s), breaks, labels = FALSE, include.lowest = TRUE,
+              right = FALSE) # Use .bincode instead of cut?
+  if (!add) {
+      plot(xy, type = type, pch = pch, cex = cex, col = col[icol], xlab = xlab,
+           ylab = ylab, asp = asp, ...)
+  } else
+      plot.xy(xy, type = type, col = col[icol], pch = pch, cex = cex, ...)
+  # if (reset) par(res$old.par)
+  return(invisible(res))
 #····································································
 }   # spoints
 
